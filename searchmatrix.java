11eab462e9888091234038226d70ee8bf8a27d5b
Ruzan Pithawala <36655798+RuzanPithawala@users.noreply.github.com>
2024-10-03 22:47:37 -0400

    Create searchmatrix.java

---
+++ searchmatrix.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int n=matrix.length;
+        int m=matrix[0].length;
+
+        int i=n-1,j=0;
+
+        while(i>=0 && j<m){
+            if(matrix[i][j]==target) return true;
+            else if(matrix[i][j]<target){
+                j++;
+            }
+            else i--;
+        }
+        return false;
+    }
+}
